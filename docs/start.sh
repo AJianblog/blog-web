9ce183712cab9934299a5afa59be5ad224e2192d
何志坚 <1305696249@qq.com>
2021-03-14 16:11:17 +0800

    update：将前端代码和后端大码分开，放在两个不同的仓库中

--- docs/start.sh
+++ docs/start.sh
@@ -4,16 +4,16 @@
 set -e
 
 # 拉取前端镜像
-docker pull registry.cn-beijing.aliyuncs.com/hezhijian/my-blog-back
+docker pull registry.cn-beijing.aliyuncs.com/hezhijian/my-blog
 # 停止前端容容器
-docker stop blog-back
+docker stop blog
 # 删除前端容器
-docker rm blog-back
+docker rm blog
 # 删除原来的前端镜像
-docker image rm blog-back
+docker image rm my-blog
 # 将拉取的前端镜像更改名字
-docker tag registry.cn-beijing.aliyuncs.com/hezhijian/my-blog:latest blog-back
+docker tag registry.cn-beijing.aliyuncs.com/hezhijian/my-blog:latest my-blog
 # 启动
-docker run --name blog-back -p 8080:8080 -d --link mysql:mysql my-blog-back
+docker run --name blog -p 4200:80 -d my-blog
 # 删除之前拉取的前端镜像
-docker image rm registry.cn-beijing.aliyuncs.com/hezhijian/my-blog-back
+docker image rm registry.cn-beijing.aliyuncs.com/hezhijian/my-blog
